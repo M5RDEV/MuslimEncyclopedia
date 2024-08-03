0ccee5143011f9e8a058472022f24e4afab2f3e9
M5R DEV <128253384+M5RDEV@users.noreply.github.com>
2024-08-03 21:22:01 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 // buttons
 let onlineDownload = document.getElementById("online-download");
 let offlineDownload = document.getElementById("offline-download");
+let fullofflineDownload = document.getElementById("full-offline-download");
 
 onlineDownload.style.cursor = "pointer";
 offlineDownload.style.cursor = "pointer";
+fullofflineDownload.style.cursor = "pointer";
 
 // API base URL
 const baseURL = "https://counting-api.onrender.com";
@@ -11,21 +13,25 @@ const baseURL = "https://counting-api.onrender.com";
 // API endpoints
 let onlineIncrementURL = `${baseURL}/downloads/online/increment`;
 let offlineIncrementURL = `${baseURL}/downloads/offline/increment`;
+let fullofflineIncrementURL = `${baseURL}/downloads/fulloffline/increment`;
 let onlineCountURL = `${baseURL}/downloads/online/count`;
 let offlineCountURL = `${baseURL}/downloads/offline/count`;
+let fullofflineCountURL = `${baseURL}/downloads/fulloffline/count`;
 
 // URLs for downloads
+let offlineURL = "https://www.mediafire.com/file/0rke8fnuc712goq/MuslimEncyclopedia_6_Reader.rar/file";
 let offlineURL = "https://download2271.mediafire.com/r01nurbmvhdgYUriI0vRqjAWCL-J0U9QiQgB0rcWao09Ex_60b_vIc6rWiQoeoTYl7Ogc6fkUTpQySW-YG-6eXSq-sZLwdHk7YSU_kNfKYVNHj02jL9WbUYAdI1iTOgRTwB8ejQLHD6Zs-rPDtZ7YCR6gcEWNxPWosWOTbOkmgAPq5g/orhjvbyzbpb3sap/Muslim+Encolopedia.rar";
 let onlineURL = "https://download1323.mediafire.com/seubp20s9osgzmWfSz9SZiGjENPIL-D9XuG9oC3nosymiQKR2tfAje4_d99UqQdl5r7uIVo6dbgQoBu3k9vM53kfA9xU44oeW9qyQA4W_J8Ve2AKASRKkj3kx5BHi-q4yyRZwNgNahmngQCJwpemMdNU4v8OdyrbA-uindqh0lIb3w/d8pk428lzfalgxa/Muslim+Encolopedia+Lite.exe";
 
 // Default counters values
 let onlineCount = 41;
 let offlineCount = 40;
+let fullofflineCount = 40;
 
 // Counters showers
 let onlineCountShower = document.getElementById("onlineCountShower");
 let offlineCountShower = document.getElementById("offlineCountShower");
-
+let fullofflineCountShower = document.getElementById("fullofflineCountShower");
 
 
 // Fetch initial download counts using Axios GET requests
@@ -42,6 +48,13 @@ axios.get(offlineCountURL)
     offlineCountShower.innerHTML = `عدد التحميلات: ${offlineCount}`;
   })
   .catch((error) => console.error("Error fetching offline download count:", error));
+
+axios.get(fullofflineCountURL)
+  .then((response) => {
+    fullofflineCount = response.data.count;
+    fullofflineCountShower.innerHTML = `عدد التحميلات: ${fullofflineCount}`;
+  })
+  .catch((error) => console.error("Error fetching full offline download count:", error));
 
 // Handle online download click
 onlineDownload.addEventListener("click", () => {
@@ -65,5 +78,13 @@ offlineDownload.addEventListener("click", () => {
     .catch((error) => console.error("Error incrementing offline download count:", error));
 });
 
-
-
+// Handle offline download click
+fullofflineDownload.addEventListener("click", () => {
+  axios.post(fullofflineIncrementURL, { type: "fulloffline" })
+    .then((response) => {
+      fullofflineCount++;
+      fullofflineCountShower.innerHTML = `عدد التحميلات: ${fullofflineCount}`;
+      window.open(fullofflineURL);
+    })
+    .catch((error) => console.error("Error incrementing full offline download count:", error));
+});
